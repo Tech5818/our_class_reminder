1f7500139658c30765e7137393d45f65addacbcf
Tech5818 <pqow5818@gmail.com>
2024-08-01 23:22:49 +0900

    feat: connect postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  /**
+   * DB가 제대로 작동하는지 테스트
+   */
+  async onModuleInit() {
+    try {
+      await this.connection.query('SELECT 1');
+      this.logger.log('✅ Database connection is success');
+    } catch (error) {
+      this.logger.error('❌ Database connection failed:', error.message);
+    }
   }
 }
